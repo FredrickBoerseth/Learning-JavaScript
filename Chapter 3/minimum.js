427d0f51a9f104527d2c8866e0db6743f70ccc68
FredrickBoerseth <fredrickboerseth@gmail.com>
2025-07-02 15:30:00 +0200

    Add files via upload

---
+++ Chapter 3/minimum.js
@@ -0,0 +1,11 @@
+function minNum(x, y) {
+    if (x < y) {console.log(x);}
+    else if (y < x) {console.log(y);}
+    else {
+        console.log("The numbers are equal");
+    }
+}
+
+minNum(4, 7);
+minNum(8, 4);
+minNum(8, 8);
